531f578e798ccaa54e9d2bc95547b900dfb4177e
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2025-09-08 15:17:36 +0900

    やけくそ

--- MultiCommentViewer/ViewModels/LoginWindow.xaml.cs
+++ MultiCommentViewer/ViewModels/LoginWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Web.WebView2.Core;
 using System;
 
-using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -28,36 +27,6 @@ namespace MultiCommentViewer.ViewModels
         {
             return _completionSource.Task;
         }
-
-        private string FindWebView2ExecutablePath()
-        {
-            var basePaths = new[]
-            {
-                @"C:\Program Files (x86)\Microsoft\EdgeWebView\Application",
-                @"C:\Program Files\Microsoft\EdgeWebView\Application"
-            };
-
-            foreach (var basePath in basePaths)
-            {
-                if (System.IO.Directory.Exists(basePath))
-                {
-                    // 最新バージョンのフォルダを探す
-                    var versionDirs = System.IO.Directory.GetDirectories(basePath)
-                        .Where(d => System.Text.RegularExpressions.Regex.IsMatch(
-                            System.IO.Path.GetFileName(d), @"^\d+\.\d+\.\d+\.\d+$"))
-                        .OrderByDescending(d => new Version(System.IO.Path.GetFileName(d)))
-                        .ToArray();
-
-                    if (versionDirs.Length > 0)
-                    {
-                        return versionDirs[0]; // 最新バージョンを返す
-                    }
-                }
-            }
-
-            return null; // 見つからない場合
-        }
-
         private async void InitializeWebView(string loginUrl)
         {
             try
@@ -180,10 +149,8 @@ namespace MultiCommentViewer.ViewModels
                         Close();
                     }
                 };
-                // 指定されたパスでWebView2環境を作成
-                var browserExecutableFolder = FindWebView2ExecutablePath();
                 var env = await CoreWebView2Environment.CreateAsync(
-                    browserExecutableFolder: browserExecutableFolder,
+                    browserExecutableFolder: "\\dll\\WebView2\\",
                     userDataFolder: _userDataFolder);
                 await WebView.EnsureCoreWebView2Async(env);
                
